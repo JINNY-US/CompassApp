3e5c83a428b8d99a428e1ff8015e71007b93459e
{Kim-KyungJin} <{kyungjin2215@gmail.com}>
2021-04-14 15:37:16 +0900

    compass app

---
+++ app/src/main/java/com/example/compassapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.compassapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.compass);
+    }
+}
